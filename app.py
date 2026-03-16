9e6225c1a27d842ce061ec6ceaf5c725b54e7fa7
Dhwani <dhwanijain731@gmail.com>
2026-03-17 00:52:47 +0530

    applied langchain textsplitter

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
 
 
 pdf_path = "docs/rag_paper.pdf"
@@ -9,9 +11,18 @@ loader = PyPDFLoader(pdf_path)
 
 documents = loader.load()
 
-
 print("Total pages:", len(documents))
-
 
 print("\nFirst page content:\n")
 print(documents[0].page_content)
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=500,
+    chunk_overlap=100
+)
+
+chunks = text_splitter.split_documents(documents)
+
+print("Total chunks:", len(chunks))
+print("\nFirst chunk:\n")
+print(chunks[0].page_content)
